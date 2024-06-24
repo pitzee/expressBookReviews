3dac94e1ce98bbae8263bbbae80dc0e909b68092
pitzee <pitzee421@gmail.com>
2024-06-24 15:46:01 -0400

    getting the book details based on the author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -35,8 +35,17 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const requestedAuthor = req.params.author; // Retrieve the author name from request parameters
+
+    // Filter books by author
+    const authorBooks = Object.values(books).filter(book => book.author === requestedAuthor);
+
+    if (authorBooks.length === 0) {
+        return res.status(404).json({ message: 'No books found for this author' });
+    }
+
+    // Return the book details
+    return res.status(200).json({ books: authorBooks });
 });
 
 // Get all books based on title
