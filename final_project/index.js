80c2c225496f9d1a664af24d81f7beb0707ccf15
pitzee <pitzee421@gmail.com>
2024-06-24 15:02:26 -0400

    authentication updated

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,18 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    const token = req.headers.authorization; // Extract the token from the request headers
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, "fingerprint_customer"); // Verify the token
+        req.user = decoded; // Attach user details to the request
+        next(); // Proceed to the next middleware
+    } catch (error) {
+        return res.status(401).json({ message: "Invalid token" });
+    }
 });
  
 const PORT =5000;
