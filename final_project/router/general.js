5e8971a6ce18a794a9dd92b474785054de522576
pitzee <pitzee421@gmail.com>
2024-06-24 15:17:28 -0400

    getting list of books

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,9 +12,11 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const bookList = Object.values(books); // Retrieve all books from the data
+
+    return res.status(200).json({ books: JSON.stringify(bookList) });
 });
+
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
